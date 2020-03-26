4299d9e372c3e0b8a835622b3d7dea395cac521c
Zablyk <zablotsky@gmail.com>
2020-03-25 21:58:21 -0400

    Added case decrement in to reducer.js and  to my dispatching action method in Counter.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,7 +6,12 @@ const reducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
         return {
             counter: state.counter + 1
-        }
+        };
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        };
     }
     return state;
 };
