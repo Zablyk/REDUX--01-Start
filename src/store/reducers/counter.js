eb56b3c69a7ecb196804488f1a260bfec76d2873
Zablyk <zablotsky@gmail.com>
2020-03-31 16:18:32 -0400

    splited my reducer.js in to counter.js and result.js and each has now data I need

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionType from './actions';
+import * as actionType from '../actions';
 
 const initialState = {
     counter: 0,
-    results: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -27,21 +26,6 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter - action.val
             };    
-        case actionType.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: state.counter})
-            };
-        case actionType.DELETE_RESULT:
-            //const id=2;
-            //const newArray = [...state.results];
-            //newArray.splice(id, 1);
-            const newArray = state.results.filter( result => result.id !== action.resultElementId);
-            return {
-                ...state,
-                results: newArray 
-            };
-                          
     }
         return state;
 };
---
+++ src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionType from '../actions';
+
+const initialState = {
+    results: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionType.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: action.result})
+            };
+        case actionType.DELETE_RESULT:
+            //const id=2;
+            //const newArray = [...state.results];
+            //newArray.splice(id, 1);
+            const newArray = state.results.filter( result => result.id !== action.resultElementId);
+            return {
+                ...state,
+                results: newArray 
+            };
+                          
+    }
+        return state;
+};
+
+export default reducer;
